ed999fd28fa68abaf0266a97373c19ce2464611a
Nasotro <lorrain.morlet@gmail.com>
2025-10-26 20:36:59 +0100

    new data aug : no rotation

--- train_vae.py
+++ train_vae.py
@@ -158,11 +158,10 @@ if __name__ == '__main__':
     img_size = cfg['image_size']
     if cfg['data_augmentation']:
         transform = transforms.Compose([
-            # transforms.Resize((img_size, img_size)),
+            transforms.RandomResizedCrop(img_size, scale=(0.7, 1.0)),
             transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(10),
+            # transforms.RandomRotation(10, fill=255),
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-            transforms.RandomResizedCrop(img_size, scale=(0.8, 1.0)),
             transforms.ToTensor(),
             # transforms.Normalize((0.5,), (0.5,))
         ])
